b37bdbcabf93e630c5492d19e8fd4218597b1191
dcrana <divyarajsinhrana2487@gmail.com>
2023-02-28 14:30:19 +0530

    form buider done

--- src/pages/DynamicFormBuilder.jsx
+++ src/pages/DynamicFormBuilder.jsx
@@ -1,20 +1,26 @@
-import { FormBuilder } from "react-formio/lib/components";
-import { useState } from "react"
+import { FormBuilder } from 'react-formio/lib/components'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 const DynamicFormBuilder = () => {
-    const [formData, setFormData] = useState({});
+    const [formData, setFormData] = useState({})
+    const preData = localStorage.getItem('schema')
+
     return (
         <div>
             <FormBuilder
-                form={{ display: "form" }}
+                form={{ display: 'form' }}
                 onChange={(schema) => {
-                    console.log(schema);
+                    console.log(schema)
+                    // setFormData({ ...formData, schema })
                     setFormData(schema);
+                    localStorage.setItem('schema', schema)
                 }}
             />
-            <Link to='/renderform' state={formData}>Go To Form</Link>
+            <Link to="/renderform" state={formData}>
+                Go To Form
+            </Link>
         </div>
-    );
+    )
 }
 
-export default DynamicFormBuilder;
+export default DynamicFormBuilder
--- src/pages/RenderForm.jsx
+++ src/pages/RenderForm.jsx
@@ -1,12 +1,17 @@
 import Form from 'react-formio/lib/components/Form';
 import { useLocation, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 const RenderForm = () => {
     const formData = useLocation().state;
     const navigate = useNavigate();
+    const storedData = localStorage.getItem("data");
+    const [prefilledData, setPreFilledData] = useState(JSON.parse(storedData) || {});
+    console.log('data>>>', prefilledData);
     return (
         <div>
-            <Form form={formData} onSubmit={(data) => {
-                console.log('data>>', data);
+            <Form form={formData} submission={prefilledData ? prefilledData : {}} onSubmit={(data) => {
+                // console.log('data>>', data);
+                localStorage.setItem('data', JSON.stringify(data));
                 navigate('/');
             }} />
         </div>
